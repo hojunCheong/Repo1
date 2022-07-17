dacd59106b79dda314b94eb3c53b8abeb440a0cc
hojunCheong <cheoung@gmail.com>
2022-07-17 09:41:47 +0900

    modify 4

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,11 +4,18 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const SERVER_URL_MAIN_PAGE = "http://localhost:3300/v1/chart/";
+let recentSearch = new Array();
+const recentSearchList = document.querySelector('#recentSearch-list');
 
 function Main() {
     const [category, setCategory] = useState('domestic');
     const [chartList, setChartList] = useState([]);
     const [time, setTime] = useState('');
+
+    // 검색기능 관련
+    const [keyWordHistory, setKeyWordHistory] = useState([]);
+    const [historyShow, setHistoryShow] = useState(false);
+    const [keywordHistoryList, setKeywordHistoryList] = useState();
     
     const searchRef = useRef('');
 
@@ -44,11 +51,21 @@ function Main() {
       setInterval(() => {setTime(setClock())}, 1000);
         // scroll event listener 등록
       window.addEventListener("scroll", handleScroll);
+      console.log('useEffect addEvent');
       return () => {
         // scroll event listener 해제
         window.removeEventListener("scroll", handleScroll);
+        console.log('useEffect removeEvent');
       };
     }, [category]);
+
+  // useEffect - 검색기록용
+  // 잠시 주석 처리
+  useEffect(() => {
+    console.log("useEffect search");
+    setKeywordHistoryList(makeKeywordHistoryList(keyWordHistory));
+  }, [keyWordHistory]);
+  
 
     // TODO [추가기능1] infinity scroll, 제거 할 때 케이스에 맞는 useEffect 로 변경 필요
     // 스크롤 이벤트 핸들러
@@ -77,12 +94,84 @@ function Main() {
 
     // TODO [추가기능2] 검색
     const searchTitle = () => {
+      // const newSearchVal = searchRef.current.value;
+      // const newSearchObj = {
+      //     id: Date.now(),
+      //     text: newSearchVal
+      // };
+      // recentSearch.push(newSearchObj);
+      // saveSearchVal();
+      // showRecentSearchVal(newSearchObj);
       console.log('input value => ' + searchRef.current.value);
+      
       const searchedList = chartList.filter((item) => {
         return item.title.toLowerCase().includes(searchRef.current.value);
       }); /* 소문자 조회 가능 / 대문자 조회 불가 */
       setChartList(searchedList);
+      addRecentSearch();
     }
+
+    // 검색 키워드 history 추가
+    const addRecentSearch = () => {
+      if (searchRef.current.value !== "") {
+        setKeyWordHistory((prev) => [...prev, searchRef.current.value]);
+      }
+      setHistoryShow(false);
+    };
+
+    // 검색기록 list 만드는 함수
+    const makeKeywordHistoryList = (data) => {
+      const historyList = data.map((item, idx) => {
+        return (
+          <div key={idx} className={styles.searchedListItem}>
+            <li
+              className={styles.keyword}
+              onClick={() => {
+                const filtered = chartList.filter((list) => {
+                  return list.title.toLowerCase().includes(item);
+                });
+                //setChartList(makeList(filtered));
+                setChartList(filtered);
+                setHistoryShow(false);
+              }}
+            >
+              {item}
+            </li>
+            {/* 검색기록 삭제버튼 */}
+            <button
+              onClick={() => {
+                const filtered = keyWordHistory.filter((keyword) => {
+                  console.log(keyword + ", " + item);
+                  return keyword != item;
+                });
+                setKeyWordHistory(filtered);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        );
+      });
+      return historyList;
+    };
+
+    /* 혹시모를 코드
+    function saveSearchVal() { //item을 localStorage에 저장합니다.
+        typeof(Storage) !== 'undefined' && localStorage.setItem("RECENT SEARCH", JSON.stringify(recentSearch));
+    };
+
+    function showRecentSearchVal(recentSrhVal) {
+      const {id, text} = recentSrhVal;
+      const item = document.createElement("li");
+      const span = document.createElement("span");
+      const button = document.createElement("button");
+      item.id = id;
+      span.innerText = text;
+      button.innerText = '❌';
+      item.appendChild(span);
+      recentSearchList.appendChild(item);
+    }
+    */
 
     const cancelSearch = () => {
       searchRef.current.value = '';
@@ -93,8 +182,21 @@ function Main() {
       if(e.key === 'Enter') searchTitle();
     }
 
+    // 검색창 클릭 event
+    const onClick = () => {
+      console.log("onClick :" + keyWordHistory);
+      // toggle 기능
+      if (keyWordHistory.length > 0) {
+        setHistoryShow(!historyShow);
+      }
+      setKeywordHistoryList(makeKeywordHistoryList(keyWordHistory));
+    };
+
     const onChange = (e) => {
+      console.log("e.target.value : " + e.target.value);
       if(e.target.value === "") cancelSearch();
+      // 실시간 검색
+      //else searchTitle();
     }
     // TODO [추가기능2] 검색
 
@@ -136,11 +238,37 @@ function Main() {
             ref={searchRef}
             type="text"
             onKeyPress={onKeyPress}
+            onClick={onClick}
             onChange={onChange}
             placeholder="제목검색" />
             <button onClick={searchTitle}>검색</button>
             <button onClick={cancelSearch}>검색취소</button>
+
+            {/* 검색기록 영역 */}
+            {historyShow && (
+              <div className={styles.historyBox}>
+                <div className={styles.history}>
+                  <ul className={styles.historyUl}>{keywordHistoryList}</ul>
+                </div>
+              </div>
+            )}
           </div>
+          {/* <div>
+            <div>
+                <h2 class="tit">최근 검색어</h2>
+                <span class="btn">모두 지우기 ❌</span>
+            </div>
+            <p class="txt"></p>
+            <ul id="recentSearch-list"></ul>
+            {recentSearch.map((item, idx) => (
+                <li key={idx}>
+                  <div>
+                    <span className={styles.title}>{item.text}</span>
+                  </div>
+                </li>
+            )) 
+            }
+        </div> */}
 
           {/* 정렬 */}
           <div>
